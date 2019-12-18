1b11487d9de575e852e1babad6d6445ab3279542
Kathir <root@packer-5d422ffb-0a5f-b7c5-1e71-5eaf6e88685a.c.pp-dev-cloudsystems.internal>
2019-12-18 03:45:44 -0800

    file writing and add quotes programmatically

--- get-quote.py
+++ get-quote.py
@@ -3,12 +3,24 @@ def main():
   #print("Keep it logically awesome.")
 
   f = open("quotes.txt")
-  quotes = f.readlines()
+  quotes = [line.strip() for line in f.readlines()]
   f.close()
 
   last = len(quotes) - 1
   rnd = random.randint(0, last)
-  print(quotes[rnd])
+  rnd1 = random.randint(0, last)
+  print((rnd+1), quotes[rnd])   
+  print((rnd1+1), quotes[rnd1])
+
+def addlines():
+  str1 = str(input("Enter the line to append to file: "))
+  #s = str(str1)
+  f=open("quotes.txt", "a+")
+  f.write(str1 + "\n")
+  f.close()
+  #print("Your new line: ", str1)
+
 
 if __name__== "__main__":
+  addlines()
   main()
